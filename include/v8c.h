9516b0eb0401eeff1286a873adb47a49f4128b6e
Evan Martin <martine@danga.com>
2008-09-30 09:58:33 -0700

    reorder to match v8.h

--- include/v8c.h
+++ include/v8c.h
@@ -1,3 +1,7 @@
+/* This file tries to follow include/v8.h in a mechanical way.  The
+   function order should match the order there, and that should
+   contain all the documentation.
+*/
 
 /* v8::Handle has a lot of nice type-checking and wrapping and unwrapping,
    but in the C world all we get is a pointer.
@@ -21,15 +25,32 @@ typedef void V8StringUtf8Value;
 typedef struct _V8TryCatch V8TryCatch;
 #endif
 
-void v8_set_flags_from_command_line(int* argc, char** argv,
-                                    bool remove_flags);
+/* Handle */
+bool v8_handle_is_empty(V8Handle handle);
 
 /* HandleScope */
 V8HandleScope* v8_handle_scope_new();
 void           v8_handle_scope_free(V8HandleScope* hs);
 
+/* Script */
+V8Handle v8_script_compile(V8Handle source);
+V8Handle v8_script_run(V8Handle script);
+
 /* String */
 V8Handle v8_string_new_utf8(const char* data, int length);
+
+/* String::Utf8Value */
+V8StringUtf8Value* v8_string_utf8_value_new(V8Handle handle);
+int v8_string_utf8_value_length(V8StringUtf8Value* utf8);
+char* v8_string_utf8_value_chars(V8StringUtf8Value* utf8);
+void v8_string_utf8_value_free(V8StringUtf8Value* utf8);
+
+/* Template */
+void v8_template_set(V8Handle tmpl, V8Handle name, V8Handle value);
+
+/* Arguments */
+int v8_arguments_length(const V8Arguments* args);
+V8Handle v8_arguments_get(const V8Arguments* args, int i);
 
 /* FunctionTemplate */
 /* V8 lets you add a "data" value to a function template, but v8c uses
@@ -41,34 +62,15 @@ V8Handle v8_function_template_new(V8InvocationCallback callback);
 /* ObjectTemplate */
 V8Handle v8_object_template_new();
 
-/* Template */
-void v8_template_set(V8Handle tmpl, V8Handle name, V8Handle value);
-
-/* Context */
-V8Handle v8_context_new(V8ExtensionConfiguration extensions,
-                        V8Handle global_template);
-void v8_context_enter(V8Handle context);
-void v8_context_exit(V8Handle context);
-
-/* Script */
-V8Handle v8_script_compile(V8Handle source);
-V8Handle v8_script_run(V8Handle script);
-
-/* Handle */
-bool v8_handle_is_empty(V8Handle handle);
-
-/* undefined */
+/* Special static values */
 V8Handle v8_undefined();
+/*V8Handle v8_null();
+V8Handle v8_true();
+V8Handle v8_true();*/
 
-/* Arguments */
-int v8_arguments_length(const V8Arguments* args);
-V8Handle v8_arguments_get(const V8Arguments* args, int i);
-
-/* Utf8Value */
-V8StringUtf8Value* v8_string_utf8_value_new(V8Handle handle);
-int v8_string_utf8_value_length(V8StringUtf8Value* utf8);
-char* v8_string_utf8_value_chars(V8StringUtf8Value* utf8);
-void v8_string_utf8_value_free(V8StringUtf8Value* utf8);
+/* V8 (class containing static functions) */
+void v8_set_flags_from_command_line(int* argc, char** argv,
+                                    bool remove_flags);
 
 /* TryCatch */
 V8TryCatch* v8_try_catch_new();
@@ -78,6 +80,13 @@ V8Handle v8_try_catch_exception(V8TryCatch* try_catch);
 V8Handle v8_try_catch_get_message(V8TryCatch* try_catch);
 void v8_try_catch_reset(V8TryCatch* try_catch);
 void v8_try_catch_set_verbose(V8TryCatch* try_catch, bool value);
+
+/* Context */
+V8Handle v8_context_new(V8ExtensionConfiguration extensions,
+                        V8Handle global_template);
+void v8_context_enter(V8Handle context);
+void v8_context_exit(V8Handle context);
+
 
 #ifdef __cplusplus
 }
