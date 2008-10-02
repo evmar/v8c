a0e0fc6b8d95d141ed0c08387cf3ba9c7520e7d4
Evan Martin <martine@danga.com>
2008-10-01 20:49:54 -0700

    clearer comments

--- include/v8c.h
+++ include/v8c.h
@@ -1,6 +1,37 @@
-/* This file tries to follow include/v8.h in a mechanical way.  The
-   function order should match the order there, and that should
-   contain all the documentation.
+/* Copyright 2007-2008 the V8 project authors. All rights reserved.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+
+       * Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above
+         copyright notice, this list of conditions and the following
+         disclaimer in the documentation and/or other materials provided
+         with the distribution.
+       * Neither the name of Google Inc. nor the names of its
+         contributors may be used to endorse or promote products derived
+         from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* v8c -- v8 API for using v8 from C (and other languages that can bind against
+   C). */
+
+/* NOTE: This file tries to follow include/v8.h directly.  The
+   function order should match the order there, and that file should contain
+   all the documentation.
 */
 
 /* v8::Handle has a lot of nice type-checking and wrapping and unwrapping,
@@ -9,6 +40,10 @@
 */
 typedef void* V8Handle;
 
+/* To have the compiler help catch type errors, this file is #included by
+   both the C++ implementation and the C user.  These typedefs make the
+   function prototypes work on both sides.
+ */
 #ifdef __cplusplus
 extern "C" {
 typedef v8::HandleScope V8HandleScope;
@@ -16,7 +51,7 @@ typedef v8::Arguments V8Arguments;
 typedef v8::ExtensionConfiguration* V8ExtensionConfiguration;
 typedef v8::String::Utf8Value V8StringUtf8Value;
 typedef v8::TryCatch V8TryCatch;
-#else  // Compiling on the C side.
+#else  /* Compiling on the C side. */
 typedef int bool;
 typedef struct _V8HandleScope V8HandleScope;
 typedef struct _V8Arguments V8Arguments;
